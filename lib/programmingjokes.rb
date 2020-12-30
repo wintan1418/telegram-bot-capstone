c053bbad5efc40e2e33eaad605aaaeafded5fac5
oluwadare juwon <wintan1418@gmail.com>
2020-12-30 19:45:57 +0100

    remove token

--- lib/programmingjokes.rb
+++ lib/programmingjokes.rb
@@ -1,12 +1,10 @@
 require_relative './recall'
-require 'giggle'
+require 'dad_jokes'
 # A class with a method which returns a joke after searching it by word! 
 class Jokes
   def random_joke(text)
-    Giggle.search(term: text, limit: 1)[0]
-  end
-
-  def search(text)
- @search = 
+    DadJokes.search(term: text, limit: 1)[0]
   end
 end
+
+ 
