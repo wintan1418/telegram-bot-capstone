959d5d123bea134d7c666a0e7cd0d56e7704abce
oluwadare juwon <wintan1418@gmail.com>
2020-12-30 11:59:52 +0100

    add gemfiles to code

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,10 @@
-require_relative '../lib/bot'
-require_relative 'telegram/bot'
 
-# MyBot.new
-p "bots is running"
+require 'telegram/bot'
+require  'movie_quotes'
+
+token = "1458568231:AAH6dSUJIyauebLVkg4MwqyYCw4RIOJeanA"
+Telegram::Bot::Client.run(token) do |bot|
+    bot.listen do |message|
+     bot.api.send_message(chat_id: message.chat.id, text: "Hello,i am a bot") 
+    end
+end
