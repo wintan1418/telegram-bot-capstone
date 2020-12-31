f1a2327aa76408e60f83819c133b8df7bf2c8b65
oluwadare juwon <wintan1418@gmail.com>
2020-12-31 09:15:17 +0100

    complete logic and add emoji commands for users

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 require_relative '../lib/bot'
 Inspbot.new
@@ -5,6 +6,6 @@ Inspbot.new
 # token = "1458568231:AAH6dSUJIyauebLVkg4MwqyYCw4RIOJeanA"
 # Telegram::Bot::Client.run(token) do |bot|
 #     bot.listen do |message|
-#      bot.api.send_message(chat_id: message.chat.id, text: "Hello,i am a bot") 
+#      bot.api.send_message(chat_id: message.chat.id, text: "Hello,i am a bot")
 #     end
 # end
