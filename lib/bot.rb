c2b66409897a62c4022607a563f2130aebf31770
oluwadare juwon <wintan1418@gmail.com>
2021-01-04 12:31:19 +0100

    fix linter errors

--- lib/bot.rb
+++ lib/bot.rb
@@ -5,36 +5,36 @@ require_relative './mood'
 require '../config'
 
 class Inspbot
-attr_reader :text
+  attr_reader :text
 
-def initialize
-  Telegram::Bot::Client.run(TOKEN_KEY) do |bot|
-    bot.listen do |message|
-      case message.text
-      when '/start'
-        bot.api.send_message(chat_id: message.chat.id, text: "Hello,i am a bot, Welcome,
+  def initialize
+    Telegram::Bot::Client.run(TOKEN_KEY) do |bot|
+      bot.listen do |message|
+        case message.text
+        when '/start'
+          bot.api.send_message(chat_id: message.chat.id, text: "Hello,i am a bot, Welcome,
         #{message.from.first_name} press /programmingjoke to get chuckling jokes to ease you and /quotes
         for inspirational quotes or '/mood' for a mood game ")
-      when '/quit'
-        bot.api.send_message(chat_id: message.chat.id, text: "#{message.from.first_name},
+        when '/quit'
+          bot.api.send_message(chat_id: message.chat.id, text: "#{message.from.first_name},
         i wish you did not press that,you can reconsider!!,bye anyways")
-      when '/programmingjoke'
-        joke = Jokes.new
-        bot.api.send_message(chat_id: message.chat.id, text: joke.random_jokes)
-      when '/quotes'
-        quote = Quotes.new
-        bot.api.send_message(chat_id: message.chat.id, text: quote.random_quotes)
+        when '/programmingjoke'
+          joke = Jokes.new
+          bot.api.send_message(chat_id: message.chat.id, text: joke.random_jokes)
+        when '/quotes'
+          quote = Quotes.new
+          bot.api.send_message(chat_id: message.chat.id, text: quote.random_quotes)
 
-      when '/mood'
-        mood = Mood.new
+        when '/mood'
+          mood = Mood.new
           bot.api.send_message(chat_id: message.chat.id, text: mood.random_mood)
 
-      else message.text != '/programmingjoke' || '/inspirationalquotes'
-            alert = "sorry,for now we have only '/quotes' and '/programmingjoke'
+        else message.text != '/programmingjoke' || '/inspirationalquotes'
+             alert = "sorry,for now we have only '/quotes' and '/programmingjoke'
                 and '/mood' for you,type either of the three to enjoy"
-            bot.api.send_message(chat_id: message.chat.id, text: alert)
+             bot.api.send_message(chat_id: message.chat.id, text: alert)
+        end
       end
     end
   end
-end
 end
